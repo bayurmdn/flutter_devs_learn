46271a29365e15409dfc9178ae17082ee2ee8857
bayurmdn <rambayu@gmail.com>
2020-12-29 21:12:01 +0700

    convert stateless to statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,31 +5,40 @@ void main() {
   //or void main () => runApp(MyApp());
 }
 
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
 
-
-class MyApp extends StatelessWidget {
+class MyAppState extends State<MyApp> {
   var questionIndex = 0;
+  var questions = ['Favorite color?', 'favorite animal?', 'Faforite test?'];
   void answerQuestion() {
-    questionIndex = questionIndex + 1; 
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
     print(questionIndex);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text('my first app'),),
-      body: Column(
-            children: [
-              Text('the question'),
-              RaisedButton(
-                child: Text('answer1'),
-                onPressed: answerQuestion,
-              ),
-              RaisedButton(
-                child: Text('answer2'),
-                onPressed: answerQuestion,
-              ),
-              RaisedButton(
-                child: Text('answer3'),
-                onPressed: answerQuestion,
-              ),
-            
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first app'),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(child: Text("answer 1"), onPressed: answerQuestion),
+            RaisedButton(child: Text("answer 2"), onPressed: answerQuestion),
+            RaisedButton(child: Text("answer 3"), onPressed: answerQuestion),
+          ],
+        ),
+      ),
+    );
+  }
+}
